4ddf107d10d870569f9a9c6e4b16a2161d3ac087
michaeltecourt <michael.tecourt@gmail.com>
2017-02-16 10:17:19 +0100

    Implement annotation detection on bean classes
    Still working on #23

--- cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/autoconfigure/annotation/AnnotationRelationshipDetector.java
+++ cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/autoconfigure/annotation/AnnotationRelationshipDetector.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.BeansException;
@@ -51,22 +52,59 @@ public abstract class AnnotationRelationshipDetector<T extends Annotation>
     public Set<Relationship> detect() {
         Set<Relationship> result = new HashSet<>();
         if (applicationContext != null) {
-            /* retrieve all beans declared in the application context */
-            String[] annotateBeans = applicationContext.getBeanDefinitionNames();
-            ConfigurableBeanFactory factory = applicationContext.getBeanFactory();
-            for (String beanName : annotateBeans) {
-                /* ... and get the bean definition of each declared beans */
-                BeanDefinition beanDefinition = factory.getMergedBeanDefinition(beanName);
-                if (beanDefinition.getSource() instanceof StandardMethodMetadata) {
-                    StandardMethodMetadata metadata = StandardMethodMetadata.class.cast(beanDefinition.getSource());
-                    processMetadata(metadata, result);
+
+            Set<Relationship> annotatedClassRels = detectAnnotatedClasses();
+            LOGGER.debug("Found {} bean classes annotated with {}", annotatedClassRels.size(), annotation);
+            result.addAll(annotatedClassRels);
+
+            Set<Relationship> annotatedFactoryMethodRels = detectAnnotatedFactoryMethods();
+            LOGGER.debug("Found {} bean factory methods annotated with {}", annotatedFactoryMethodRels.size(),
+                    annotation);
+            result.addAll(annotatedFactoryMethodRels);
+        }
+        return result;
+    }
+
+    /**
+     * Detected relationships in annotated bean classes.
+     * 
+     * @return Relationships detected from annotated classes.
+     */
+    protected Set<Relationship> detectAnnotatedClasses() {
+        Set<Relationship> result = new HashSet<>();
+        String[] annotatedBeans = applicationContext.getBeanNamesForAnnotation(annotation);
+        for (String beanName : annotatedBeans) {
+            T anno = applicationContext.findAnnotationOnBean(beanName, annotation);
+            result.add(extractFromAnnotation(anno));
+        }
+        return result;
+    }
+
+    /**
+     * Detect relationships in annotated {@link Bean @Bean} Factory methods.
+     * 
+     * @return Relationships detected from factory methods.
+     */
+    protected Set<Relationship> detectAnnotatedFactoryMethods() {
+        Set<Relationship> result = new HashSet<>();
+        /* retrieve all beans declared in the application context */
+        String[] annotateBeans = applicationContext.getBeanDefinitionNames();
+        ConfigurableBeanFactory factory = applicationContext.getBeanFactory();
+        for (String beanName : annotateBeans) {
+            /* ... and get the bean definition of each declared beans */
+            BeanDefinition beanDefinition = factory.getMergedBeanDefinition(beanName);
+            if (beanDefinition.getSource() instanceof StandardMethodMetadata) {
+                StandardMethodMetadata metadata = StandardMethodMetadata.class.cast(beanDefinition.getSource());
+                Optional<Relationship> rel = detectMethodMetadata(metadata);
+                if (rel.isPresent()) {
+                    result.add(rel.get());
                 }
             }
         }
         return result;
     }
 
-    private void processMetadata(final StandardMethodMetadata metadata, final Set<Relationship> result) {
+    protected Optional<Relationship> detectMethodMetadata(final StandardMethodMetadata metadata) {
         /*
          * ... get the metadata of the current definition bean for the
          * annotation DependencyHint. In this case, we retrieve the annotation
@@ -77,11 +115,13 @@ public abstract class AnnotationRelationshipDetector<T extends Annotation>
          * ... get the Hint directly from the class (Target = ElementType.TYPE)
          */
         T annotation = getAnnotation(metadata);
+        Relationship rel = null;
         if (hintData != null && !hintData.isEmpty()) {
-            result.add(extractFromAnnotationAttributes(hintData));
+            rel = extractFromAnnotationAttributes(hintData);
         } else if (annotation != null) {
-            result.add(extractFromAnnotation(annotation));
+            rel = extractFromAnnotation(annotation);
         }
+        return Optional.ofNullable(rel);
     }
 
     /**
