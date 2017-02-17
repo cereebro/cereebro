6d8d51500be6accbf273429f0ea602febdba0d43
michaeltecourt <michael.tecourt@gmail.com>
2017-02-17 14:48:42 +0100

    Add cereebro-spring module including ResourceSnitch impl
    Implement a Spring `Resource` based `Snitch` that reads 
    JSON content from a resource, using a JSON `ObjectMapper`.
    
    No need to decouple serialization at this point.

---
+++ cereebro-spring/src/main/java/io/cereebro/spring/ResourceSnitch.java
@@ -0,0 +1,72 @@
+package io.cereebro.spring;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Objects;
+
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.cereebro.core.Snitch;
+import io.cereebro.core.SnitchingException;
+import io.cereebro.core.SystemFragment;
+
+/**
+ * Snitch backed by a static Spring {@link Resource}. The resource must contain
+ * a valid JSON representation of a {@link SystemFragment}.
+ * 
+ * @author michaeltecourt
+ */
+public class ResourceSnitch implements Snitch {
+
+    private final ObjectMapper objectMapper;
+    private final Resource resource;
+
+    /**
+     * Snitch backed by a static Spring {@link Resource}. The resource must
+     * contain a valid JSON representation of a {@link SystemFragment}.
+     * 
+     * @param mapper
+     *            Jackson object mapper
+     * @param resource
+     *            Resource containing a JSON SystemFragment.
+     */
+    public ResourceSnitch(ObjectMapper mapper, Resource resource) {
+        this.resource = Objects.requireNonNull(resource, "Resource required");
+        this.objectMapper = Objects.requireNonNull(mapper, "ObjectMapper required");
+    }
+
+    /**
+     * Snitch backed by a static Spring {@link Resource}. The resource must
+     * contain a valid JSON representation of a {@link SystemFragment}.
+     * 
+     * @param mapper
+     *            Jackson object mapper
+     * @param resource
+     *            Resource containing a JSON SystemFragment.
+     * @return ResourceSnitch
+     */
+    public static ResourceSnitch of(ObjectMapper mapper, Resource resource) {
+        return new ResourceSnitch(mapper, resource);
+    }
+
+    @Override
+    public URI getLocation() {
+        try {
+            return resource.getURI();
+        } catch (IOException e) {
+            throw new IllegalStateException("Error reading Snitch Resource URI : " + resource.getDescription(), e);
+        }
+    }
+
+    @Override
+    public SystemFragment snitch() {
+        try {
+            return objectMapper.readValue(resource.getInputStream(), SystemFragment.class);
+        } catch (IOException e) {
+            throw new SnitchingException(getLocation(), "Error while reading resource content", e);
+        }
+    }
+
+}
---
+++ cereebro-spring/src/test/java/io/cereebro/spring/ResourceSnitchTest.java
@@ -0,0 +1,98 @@
+package io.cereebro.spring;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.cereebro.core.Component;
+import io.cereebro.core.ComponentRelationships;
+import io.cereebro.core.Consumer;
+import io.cereebro.core.Dependency;
+import io.cereebro.core.SnitchingException;
+import io.cereebro.core.SystemFragment;
+
+/**
+ * {@link ResourceSnitch} unit tests.
+ * 
+ * @author michaeltecourt
+ */
+public class ResourceSnitchTest {
+
+    private static final String RESOURCE_PATH = "/system-fragment-gambit.json";
+    private ClassPathResource classpathResource;
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        classpathResource = new ClassPathResource(RESOURCE_PATH);
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void unreadableResourceURIShouldThrowIllegalStateException() throws IOException {
+        Resource resourceMock = Mockito.mock(Resource.class);
+        Mockito.when(resourceMock.getURI()).thenThrow(new IOException("unit test"));
+        ResourceSnitch snitch = ResourceSnitch.of(objectMapper, resourceMock);
+        try {
+            snitch.getLocation();
+            Assert.fail("Expected IllegalStateException");
+        } catch (IllegalStateException e) {
+            // ok
+            Mockito.verify(resourceMock).getURI();
+        }
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void errorReadingResourceShouldThrowSnitchingException() throws IOException {
+        ObjectMapper objectMapperMock = Mockito.mock(ObjectMapper.class);
+        Mockito.when(objectMapperMock.readValue(Mockito.any(InputStream.class), Mockito.any(Class.class)))
+                .thenThrow(new IOException("unit test"));
+        ResourceSnitch snitch = ResourceSnitch.of(objectMapperMock, classpathResource);
+        try {
+            snitch.snitch();
+            Assert.fail("Expected SnitchingException");
+        } catch (SnitchingException e) {
+
+        }
+
+    }
+
+    @Test
+    public void parse() {
+        // @formatter:off
+        ComponentRelationships rel = ComponentRelationships.builder()
+                .component(Component.of("gambit", "superhero"))
+                .addDependency(Dependency.on(Component.of("rogue", "superhero")))
+                .addDependency(Dependency.on(Component.of("cards", "addiction")))
+                .addConsumer(Consumer.by(Component.of("apocalypse", "villain")))
+                .addConsumer(Consumer.by(Component.of("angel", "superhero")))
+                .build();
+        // @formatter:on
+        SystemFragment expected = SystemFragment.of(rel);
+
+        ResourceSnitch snitch = ResourceSnitch.of(objectMapper, new ClassPathResource(RESOURCE_PATH));
+        SystemFragment actual = snitch.snitch();
+        Assert.assertEquals(expected, actual);
+    }
+
+    /**
+     * The actual URI returned by Spring Resource is the file absolute path,
+     * which we don't know for sure. We just make sure the relative path is part
+     * of the absolute URI.
+     */
+    @Test
+    public void locationShouldMatchResourceURI() {
+        ResourceSnitch snitch = ResourceSnitch.of(objectMapper, new ClassPathResource(RESOURCE_PATH));
+        Assert.assertTrue(snitch.getLocation().toString().contains(RESOURCE_PATH));
+    }
+
+}
