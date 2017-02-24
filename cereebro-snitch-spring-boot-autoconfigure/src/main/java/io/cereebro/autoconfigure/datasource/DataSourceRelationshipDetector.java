05e1a8bd9e4f6d630448a3172576e656f2c9bed3
lucwarrot <warrotluc@gmail.com>
2017-02-24 15:14:39 +0100

    detect the datasources after the creation of the bean
    closes #35

--- cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/autoconfigure/datasource/DataSourceRelationshipDetector.java
+++ cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/autoconfigure/datasource/DataSourceRelationshipDetector.java
@@ -1,9 +1,11 @@
 package io.cereebro.autoconfigure.datasource;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
@@ -11,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.cereebro.core.Component;
+import io.cereebro.core.ComponentType;
 import io.cereebro.core.Dependency;
 import io.cereebro.core.Relationship;
 import io.cereebro.core.RelationshipDetector;
@@ -30,13 +33,23 @@ public class DataSourceRelationshipDetector implements RelationshipDetector {
     @Autowired(required = false)
     private Set<DataSource> dataSource;
 
+    private Set<Relationship> relations = Collections.emptySet();
+
+    @PostConstruct
+    protected void postConstruct() {
+        relations = detect();
+    }
+
     @Override
     public Set<Relationship> detect() {
-        final Set<Relationship> result = new HashSet<>();
+        if (!relations.isEmpty()) {
+            return relations;
+        }
         if (dataSource != null) {
+            final Set<Relationship> result = new HashSet<>();
             for (DataSource ds : dataSource) {
                 try {
-                    result.add(Dependency.on(Component.of(ds.getConnection().getCatalog(), "DATABASE")));
+                    result.add(Dependency.on(Component.of(ds.getConnection().getCatalog(), ComponentType.DATABASE)));
                 } catch (SQLException e) {
                     if (LOGGER.isInfoEnabled()) {
                         LOGGER.info("could not fetch the default catalog of the database connection");
@@ -46,8 +59,9 @@ public class DataSourceRelationshipDetector implements RelationshipDetector {
                     }
                 }
             }
+            relations = result;
         }
-        return result;
+        return relations;
     }
 
 }
