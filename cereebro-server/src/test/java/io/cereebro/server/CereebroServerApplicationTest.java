183aafadd06a09b073e31fc3a6ca8ef979be472d
michaeltecourt <michael.tecourt@gmail.com>
2017-03-19 17:22:11 +0200

    License header : fix inception year
    Closes #43
    (for real)

--- cereebro-server/src/test/java/io/cereebro/server/CereebroServerApplicationTest.java
+++ cereebro-server/src/test/java/io/cereebro/server/CereebroServerApplicationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009 the original authors (http://cereebro.io)
+ * Copyright © 2017 the original authors (http://cereebro.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
