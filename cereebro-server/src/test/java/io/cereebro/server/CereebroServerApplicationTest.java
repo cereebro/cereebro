a039c409adb4883a902972abe2068d468130e189
michaeltecourt <michael.tecourt@gmail.com>
2017-04-21 17:15:19 +0200

    Test fix

--- cereebro-server/src/test/java/io/cereebro/server/CereebroServerApplicationTest.java
+++ cereebro-server/src/test/java/io/cereebro/server/CereebroServerApplicationTest.java
@@ -61,7 +61,7 @@ public class CereebroServerApplicationTest {
             .then()
                 .statusCode(200)
                 .contentType(ContentType.HTML)
-                .body("html.body.div.h1", Matchers.is("cereebro-system"));
+                .body("html.body.div.div.h1", Matchers.is("cereebro-system"));
                 // The expression works, but can't guess the order when using sets
                 // .body("html.body.div.table.tbody.tr[0].td[0]", Matchers.is("gambit"))
         // @formatter:on
