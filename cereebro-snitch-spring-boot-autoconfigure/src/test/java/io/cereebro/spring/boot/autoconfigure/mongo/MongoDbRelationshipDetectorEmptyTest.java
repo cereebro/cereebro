1a545088dec45a5a21b0710eee4c65626f9c44b4
michaeltecourt <michael.tecourt@gmail.com>
2017-04-24 22:41:03 +0200

    Fix Mongo Database relationship detector
    Use `MongoClient#listDatabaseNames()` to find out 
    the name of Mongo Databases instead of only used ones.
    Use embedded MongoDB for integration test instead of 
    mocked `MongoClient`.
    
    Fixes #65

--- cereebro-snitch-spring-boot-autoconfigure/src/test/java/io/cereebro/spring/boot/autoconfigure/mongo/MongoDbRelationshipDetectorEmptyTest.java
+++ cereebro-snitch-spring-boot-autoconfigure/src/test/java/io/cereebro/spring/boot/autoconfigure/mongo/MongoDbRelationshipDetectorEmptyTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -37,7 +38,7 @@ public class MongoDbRelationshipDetectorEmptyTest {
         Assertions.assertThat(detector.detect()).isEmpty();
     }
 
-    @SpringBootApplication
+    @SpringBootApplication(exclude = MongoAutoConfiguration.class)
     public static class MissingMongoDbTestApplication {
 
     }
