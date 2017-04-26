645ce66ce6a8ddc68eb000a030fdc272d6ea47de
michaeltecourt <michael.tecourt@gmail.com>
2017-04-26 11:03:29 +0200

    Simplify detection rules, fix the build (WIP)
    If there is at least one Neo4j session available, 
    create a single dependency with a default name.
    
    Add Apache License headers.
    
    WIP : Tests missing

--- cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/spring/boot/autoconfigure/neo4j/Neo4jRelationshipDetector.java
+++ cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/spring/boot/autoconfigure/neo4j/Neo4jRelationshipDetector.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2017 the original authors (http://cereebro.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cereebro.spring.boot.autoconfigure.neo4j;
 
 import java.util.ArrayList;
@@ -7,11 +22,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.neo4j.ogm.session.Session;
-import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-
-import com.google.common.collect.Sets;
 
 import io.cereebro.core.Component;
 import io.cereebro.core.ComponentType;
@@ -19,13 +30,29 @@ import io.cereebro.core.Dependency;
 import io.cereebro.core.Relationship;
 import io.cereebro.core.RelationshipDetector;
 
+/**
+ * Neo4J database relationship detector. Databases don't have names in Neo4j so
+ * we use a default value whenever at least one Neo4j {@link Session} is
+ * available.
+ * 
+ * @author lucwarrot
+ * @author michaeltecourt
+ */
 public class Neo4jRelationshipDetector implements RelationshipDetector {
 
-    private Neo4jProperties properties;
+    private static final String DEFAULT_NAME = "default";
 
     private final List<Session> sessions;
 
-    public Neo4jRelationshipDetector(Collection<Session> sessions, Neo4jProperties properties) {
+    /**
+     * Neo4J database relationship detector. Databases don't have names in Neo4j
+     * so we use a default value whenever at least one Neo4j {@link Session} is
+     * available.
+     * 
+     * @param sessions
+     *            Neo4j sessions available in the application context.
+     */
+    public Neo4jRelationshipDetector(Collection<Session> sessions) {
         this.sessions = new ArrayList<>();
         if (!CollectionUtils.isEmpty(sessions)) {
             this.sessions.addAll(sessions);
@@ -34,14 +61,8 @@ public class Neo4jRelationshipDetector implements RelationshipDetector {
 
     @Override
     public Set<Relationship> detect() {
-        if (sessions.size() == 1 && !StringUtils.isEmpty(properties.getUri())) {
-            return Sets.newHashSet(Dependency.on(Component.of(properties.getUri(), ComponentType.NEO4J)));
-        }
-        final Set<Relationship> result = new HashSet<>(sessions.size());
-        for (int i = 0; i < sessions.size(); i++) {
-            result.add(Dependency.on(Component.of("default_" + i, ComponentType.NEO4J)));
-        }
-        return result;
+        return sessions.isEmpty() ? new HashSet<>()
+                : Dependency.on(Component.of(DEFAULT_NAME, ComponentType.NEO4J)).asRelationshipSet();
     }
 
 }
--- cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/spring/boot/autoconfigure/neo4j/Neo4jRelationshipDetectorAutoConfiguration.java
+++ cereebro-snitch-spring-boot-autoconfigure/src/main/java/io/cereebro/spring/boot/autoconfigure/neo4j/Neo4jRelationshipDetectorAutoConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2017 the original authors (http://cereebro.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cereebro.spring.boot.autoconfigure.neo4j;
 
 import java.util.List;
@@ -5,7 +20,6 @@ import java.util.List;
 import org.neo4j.ogm.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,10 +29,8 @@ public class Neo4jRelationshipDetectorAutoConfiguration {
     @Autowired(required = false)
     private List<Session> neo4jSessions;
 
-    @Autowired(required = false)
-    private Neo4jProperties neo4jProperties;
-
     public Neo4jRelationshipDetector neo4jDetector() {
-        return new Neo4jRelationshipDetector(neo4jSessions, neo4jProperties);
+        return new Neo4jRelationshipDetector(neo4jSessions);
     }
+
 }
