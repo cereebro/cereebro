0bc94a2c55dc3a18929c68a9d3e924ca9375befc
michaeltecourt <michael.tecourt@gmail.com>
2017-08-03 16:27:05 +0200

    Fix standalone Cereebro Server
    Change the order annotation on 
     `CereebroEurekaServerAddOnAutoConfiguration` to reference the 
    configuration class as a String because it may be absent from 
    the classpath, causing obscure `java.lang.ArrayStoreException`s.
    
    Closes #92

--- cereebro-server/src/main/java/io/cereebro/server/eureka/CereebroEurekaServerAddOnAutoConfiguration.java
+++ cereebro-server/src/main/java/io/cereebro/server/eureka/CereebroEurekaServerAddOnAutoConfiguration.java
@@ -18,7 +18,6 @@ package io.cereebro.server.eureka;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,7 +38,7 @@ import io.cereebro.core.SnitchRegistry;
  */
 @Configuration
 @ConditionalOnClass(EurekaServerContext.class)
-@AutoConfigureAfter(EurekaServerAutoConfiguration.class)
+@AutoConfigureAfter(name = "org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration")
 public class CereebroEurekaServerAddOnAutoConfiguration {
 
     @Bean
